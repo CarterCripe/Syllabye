5f98c170354d82fcacafdfa874364222caffd72e
Brennan Duman <dumanb@oregonstate.edu>
2026-02-17 15:15:30 -0800

    Began laying groundwork for file input

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -42,12 +42,44 @@ function switchScreen(origin, dest){
 }
 
 /*
+ * extracts the file name from the given path
+ * SOURCE: HTML SPEC: https://html.spec.whatwg.org/multipage/input.html#fakepath-srsly
+ * Param: path {str} - the file path of an inputted file
+ */
+function extractFilename(path) {
+  if (path.substr(0, 12) == "C:\\fakepath\\")
+    return path.substr(12); // modern browser
+  var x;
+  x = path.lastIndexOf('/');
+  if (x >= 0) // Unix-based path
+    return path.substr(x+1);
+  x = path.lastIndexOf('\\');
+  if (x >= 0) // Windows-based path
+    return path.substr(x+1);
+  return path; // just the filename
+}
+
+function processFile(filePath){
+  let fileName = extractFilename(filePath);
+
+  
+}
+
+function addSyllabus(){
+  //TODO: see what kind of data has been inputted (.pdf, .txt, raw text), and
+  //  process accordingly
+
+
+}
+
+/*
  * -------------------------
  * Listen Events
  * -------------------------
  */
 
-// 
+// Add Syllabus
+
 
 
 // Home Screen Screen Switching Buttons
